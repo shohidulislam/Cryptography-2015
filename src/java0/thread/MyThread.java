45e7f6c7751ac5594d8ad05b8976adc8a85fd1ae
java0-cloudcentrik <java0.cloudcentrik@gmail.com>
2015-10-04 17:52:24 +0200

    thread package added

---
+++ src/java0/thread/MyThread.java
@@ -0,0 +1,22 @@
+package java0.thread;
+
+public class MyThread extends Thread {
+	private int startIdx, nThreads, maxIdx;
+
+	   public MyThread(int s, int n, int m)
+	   {
+	      this.startIdx = s;
+	      this.nThreads = n;
+	      this.maxIdx = m;
+	   }
+
+	   @Override
+	   public void run()
+	   {
+	      for(int i = this.startIdx; i < this.maxIdx; i += this.nThreads)
+	      {
+	         System.out.println("[ID " + this.getId() + "] " + i);
+	      }
+	   }
+
+}
