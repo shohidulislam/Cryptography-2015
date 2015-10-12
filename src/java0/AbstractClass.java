b05d2ef874bb1d85e9941fdf2e1e15b65601f9d1
info.cloudcentrik <info.cloudcentrik@gmail.com>
2015-10-12 18:47:36 +0200

    "Jahir vi request test"

---
+++ src/java0/AbstractClass.java
@@ -0,0 +1,12 @@
+package java0;
+
+public abstract class AbstractClass {
+
+	private int i=0;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/java0/thread/GithubTest.java
@@ -0,0 +1,12 @@
+package java0.thread;
+
+public class GithubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test");
+
+	}
+
+}
