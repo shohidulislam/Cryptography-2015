676f15c7be6f64235b25a19fe5da92c124f3cc68
shohidulislam <comwizard07@gmail.com>
2015-11-17 04:06:48 +0100

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,44 @@
+public class Fact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I am using GitHub");
+		System.out.println("I am using GitHub2");
+		
+		boolean bb = false;
+		int r = 1; 
+		int v = 0;
+		
+		if (!bb && (r != v))
+			System.out.println("T==V");
+		String str = "anbcd" + 5;
+		int y = 10;
+
+		int z = 3, t, a,b, c;
+		a = b = c = 5;
+		
+		a = b + c;		
+
+		t = y++;
+		z+= ++y * y+++5;
+
+		(t)+=0;
+		System.out.println(z);
+
+		System.out.println(str);
+		System.out.println(a);
+		
+
+	
+		
+		//		System.out.println("I am using GitHub2");
+		//		System.out.println("I am using GitHub2");
+		//		System.out.println("I am using GitHub2");
+		//		System.out.println("I am using GitHub2");
+		//		System.out.println("I am using GitHub2");
+
+		//		obj1.setVisible(obj1);
+	}
+
+}
